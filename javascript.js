9cebca5fbe6eaf680f7ccbafbaa445f264bf7d5d
nighfy <82936194+nighfy@users.noreply.github.com>
2023-01-07 13:29:36 +0100

    Add CSS and code to colour squares

--- javascript.js
+++ javascript.js
@@ -1,11 +1,32 @@
+createGrid(16);
+
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+    square.addEventListener('mousemove', () => {
+        square.classList.add('coloured');
+    }, {once: true}) 
+});
+
 function createGrid(size) {
     const container = document.createElement('div');
     container.classList.add('container');
 
     for(let i = 0; i < size*size; i++) {
         const temporary = document.createElement('div');
-        temporary.textContent = 'X';
+        temporary.classList.add('square');
+        temporary.textContent = i + 1;
         container.appendChild(temporary);
     }
     document.body.prepend(container);
+    
+    const stylesheet = document.styleSheets[0];
+    for(let i = 0; i < stylesheet.cssRules.length; i++) {
+        if(stylesheet.cssRules[i].selectorText === 'div.container div') {
+            elementRules = stylesheet.cssRules[i];
+        }
+    }
+
+    elementRules.style.setProperty('height', `${1000/size}px`);
+    elementRules.style.setProperty('width', `${1000/size}px`);
 }
+
